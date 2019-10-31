db0c8228421f8ed9182ae57f6b1efbb4f04125a0
phuvuong95 <47914479+phuvuong95@users.noreply.github.com>
2019-10-31 10:37:45 +0700

    ArrowFunction(Part1)

---
+++ ArrowFunction(Part 1)/index.js
@@ -0,0 +1,35 @@
+// arrow function expression
+// fat arrow
+
+// function sum (a, b) {
+//     return a + b;
+// }
+// console.log(sum(1, 2)); //3
+
+
+// var sum2 = function(a, b) {
+//     return a + b;
+// };
+
+// console.log(sum(2, 3)); //5
+
+// var sum = (a, b) => {
+//     return a + b;
+// };
+
+
+// var sum = (a, b) => a + b;
+// console.log(sum(4, 6));
+
+// var square = (x) => x*x; Cách 1
+// var square = x => x*x; //Cách 2
+// console.log(square(5));
+
+
+var arr = [1, 2, 3];
+arr.map(x => x*x); //output: [1, 4, 9]
+
+
+
+
+
