c43ed4fed8d443e38505156a03385944f23c8472
phuvuong95 <47914479+phuvuong95@users.noreply.github.com>
2019-11-01 09:48:50 +0700

    Class

---
+++ Class/index.js
@@ -0,0 +1,24 @@
+// constructor function
+// function Mouse(name) {
+//     this.name = name;
+// }
+
+// Mouse.prototype.run = function() {
+//     console.log(`$(this.name) is running`);
+// }
+
+class Mouse {
+    constructor(name){
+        this.name = name;
+    }
+
+    run() {
+        console.log(`$(this.name) is running`);
+    }
+}
+
+
+
+const mouse = Mouse('Mickey');
+mouse.run();
+
