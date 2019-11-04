e33366462c65d416fa04efb0e4b91b5e34ba0089
phuvuong95 <47914479+phuvuong95@users.noreply.github.com>
2019-11-04 09:39:25 +0700

    ValueTypes and ReferenceTypes

--- ValueTypes-ReferenceTypes/index.js
+++ ValueTypes-ReferenceTypes/index.js
@@ -6,3 +6,16 @@ let obj1 = { a: 1};
 let obj2 = { a: 1};
 console.log(obj1 === obj2); //false
 
+let a3 = a2;
+console.log(a3 === a2);
+a3 = 2;
+console.log(a3 === a2);
+
+
+let obj3 = ob2;
+console.log(obj3 === obj2);
+ob3.a = 2;
+console.log(obj3 === obj2);
+
+
+
